5fde0a4fc8ad5c34af2090600938fe6b0473ab44
friedkorn <sridhardmitmanipal@gmail.com>
2026-03-29 23:07:44 +0530

    Create LargestPalindromeProduct.cpp

---
+++ LargestPalindromeProduct.cpp
@@ -0,0 +1,32 @@
+//Author:CelestialRex
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll= long long int;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPal(int x) {
+    string s = to_string(x);
+    string p = string(s.rbegin(),s.rend());
+    if (s==p)
+        return true;
+    return false;
+}
+
+void solve(){
+    int ans=0;
+    for (int i=100;i<=999;i++)
+        for (int j=i;j<=999;j++)
+            if (isPal(i*j))
+                ans = max(ans,i*j);
+    cout<<ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
